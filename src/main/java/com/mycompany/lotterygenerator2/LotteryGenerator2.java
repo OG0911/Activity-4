35a80fc64ba05992b1d20aa3f2f11d4558a72030
Oageng Mabitsela [Learnership - Curro Private College Cape Town - All Schools] <oageng.m@curro.co.za>
2024-10-22 10:22:33 +0200

    Second commit

--- src/main/java/com/mycompany/lotterygenerator2/LotteryGenerator2.java
+++ src/main/java/com/mycompany/lotterygenerator2/LotteryGenerator2.java
@@ -13,21 +13,21 @@ public class LotteryGenerator2 {
     public static void main(String[] args) {
         
         int num1, num2, num3, num4, num5, powerball;
-        num1 = (int) ((Math.random() * 30) + 1);
+        num1 = (int) ((Math.random() * 51) + 1);
 
-        do {num2 = (int) ((Math.random() * 30) + 1); 
+        do {num2 = (int) ((Math.random() * 51) + 1); 
         } while (num1 == num2);
         
-        do {num3 = (int) ((Math.random() * 30) + 1); 
+        do {num3 = (int) ((Math.random() * 51) + 1); 
         } while ((num1 == num2) || (num2 == num3));
         
-        do {num4 = (int) ((Math.random() * 30) + 1); 
+        do {num4 = (int) ((Math.random() * 51) + 1); 
         } while ((num1 == num2) || (num2 == num3) || (num3 == num4));
         
-        do {num5 = (int) ((Math.random() * 30) + 1); 
+        do {num5 = (int) ((Math.random() * 51) + 1); 
         } while ((num1 == num2) || (num1 == num2) || (num3 == num4) || (num4== num5));
         
-        do {powerball = (int) ((Math.random() * 30) + 1); 
+        do {powerball = (int) ((Math.random() * 51) + 1); 
         } while ((num1 == num2) || (num1 == num2) || (num3 == num4) || (num4== num5) || (num5== powerball));
         
         JOptionPane.showMessageDialog(null, "Lottery numbers: " + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + ", "  +  "\nPowerball: " + powerball);
