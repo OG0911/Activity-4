ff3c330af325bdf221e33719bbff797e54a016d6
Oageng Mabitsela [Learnership - Curro Private College Cape Town - All Schools] <oageng.m@curro.co.za>
2024-10-22 10:01:05 +0200

    Intial commit

---
+++ src/main/java/com/mycompany/lotterygenerator2/LotteryGenerator2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lotterygenerator2;
+import javax.swing.*;
+/**
+ *
+ * @author OAGENG.M
+ */
+public class LotteryGenerator2 {
+
+    public static void main(String[] args) {
+        
+        int num1, num2, num3, num4, num5, powerball;
+        num1 = (int) ((Math.random() * 30) + 1);
+
+        do {num2 = (int) ((Math.random() * 30) + 1); 
+        } while (num1 == num2);
+        
+        do {num3 = (int) ((Math.random() * 30) + 1); 
+        } while ((num1 == num2) || (num2 == num3));
+        
+        do {num4 = (int) ((Math.random() * 30) + 1); 
+        } while ((num1 == num2) || (num2 == num3) || (num3 == num4));
+        
+        do {num5 = (int) ((Math.random() * 30) + 1); 
+        } while ((num1 == num2) || (num1 == num2) || (num3 == num4) || (num4== num5));
+        
+        do {powerball = (int) ((Math.random() * 30) + 1); 
+        } while ((num1 == num2) || (num1 == num2) || (num3 == num4) || (num4== num5) || (num5== powerball));
+        
+        JOptionPane.showMessageDialog(null, "Lottery numbers: " + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + ", "  +  "\nPowerball: " + powerball);
+        
+        }  
+    }
+
